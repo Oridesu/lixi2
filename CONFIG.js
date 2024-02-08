193884667cedaec825ca5195d0636a002224df17
Oridesu <endervn844@gmail.com>
2024-02-08 18:55:41 +0800

    lixiver2

---
+++ CONFIG.js
@@ -0,0 +1,10 @@
+const CONFIGDATA = {
+  titleHeader: "Tết Đến rồi. </br> Mau lì xì tao đi!",
+  descriptionHeader:
+    "Đứa nào Không lì xì Làm chó .",
+  buttonYes: "Ok",
+  buttonNo: "Đéo",
+  titleModar: "Cảm ơn ",
+  descriptionModar:
+    "Đây là số tk. </br> 12312311231231123 </br> Mã Ngân Hàng: 123</br> Love you <3",
+};
